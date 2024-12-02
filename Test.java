0b44606550daaeaa160db607a5cbb02aaa3fa47c
APARANJINI PALLELA <aparanjinipallela1@gmail.com>
2024-12-02 11:36:06 +0530

    java code

---
+++ Test.java
@@ -0,0 +1,7 @@
+class Test:
+public static void main(String args[])
+{
+    System.out.println("This is a sample java code");
+    int a=10,b=20;
+    System.out.println("Addition of 10 and 20:",a+b);
+}
